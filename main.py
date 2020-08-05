b2c1d49d1239d0a2a0220dd35f95eecd31d561da
Guo Wenjiao <1767397736@qq.com>
2020-08-05 13:14:46 +0800

    upad选择功能

--- main.py
+++ main.py
@@ -4,28 +4,53 @@ from psychopy import visual, core, event, gui
 import random,os,time
 import pandas as pd
 
+# 获得某路径csv文件名list
+def file_name(file_dir):
+    L=[]
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.csv':
+                # L.append(os.path.join(root, file))
+                L.append(os.path.splitext(file)[0])
+    return L
+# 老师选择一下嘛
+def Get_Class(class_list):
+    myDlg = gui.Dlg(title="Choice Class")
+    myDlg.addText("---------------Choice Class-------------")
+    myDlg.addField('Class:',choices=class_list)
+    myDlg.show()
+    if myDlg.OK:
+        thisInfo = myDlg.data
+    else:
+        exit(0)
+    choiced_class = {}
+    choiced_class['Class'] = thisInfo[0]
+    return choiced_class
 
-# 中文字体
-text_font = 'SimSun'
-winsize = [1920,1080]
-win = visual.Window(winsize,fullscr=True,color='#D0D0D0',units='pix')
+
 def drawimage(image1,location = [-290,-12.5]):
     pic = visual.ImageStim(win, image= image1, pos=location)
     pic.draw(win)
 def drawText(text,location=[0,0],color=[-1, -1, -1],height= 50):
     text = visual.TextStim(win, text=text, pos=location,color=color,height=height,font=text_font)
     text.draw(win)
-
-
-
-# 读文件
+# 选择、读取文件
 path = os.path.abspath('.')
-df = pd.read_csv(path+'/num_file/xuehao.csv', encoding='utf-8-sig')
+class_list = file_name(path)
+choiced_class = Get_Class(class_list)
+df = pd.read_csv(path+'/num_file/'+ choiced_class['Class'] + '.csv', encoding='utf-8-sig')
+print(df)
+
 num_list = []
 num_list_file = df['学号']
 for j in num_list_file:
     num_list.append(j)
 print(len(num_list))
+# 中文字体 窗口
+text_font = 'SimSun'
+winsize = [1920,1080]
+win = visual.Window(winsize,fullscr=True,color='#D0D0D0',units='pix')
+
 text = '1'
 text_list = []
 # text_list = ['\n','\n','\n','\n','\n','\n','\n','\n','\n','\n','\n','\n','\n','\n']
@@ -76,42 +101,6 @@ for i in range(3000):
         #     break
     if thisResp == True:    # time.sleep(5)
             break
-    # elif len(text_list) > chengxianhangshu:
-    #     # if locationx %(height/2) != 0 or is_add == True:
-    #     if locationx %(height/2) != 0:
-    #         locationx += move_juli
-    #         #shishi
-    #         drawText(text,[0,locationx -xiaxian])
-    #         print(text,locationx)
-    #         win.flip()
-    #         time.sleep(stop_time)
-    #         is_add = False
-    #
-    #     elif locationx %(height/2) == 0 :
-    #         if is_add == False:
-    #             text_list.pop(0)
-    #             selected_num = random.choice(list(num_list))
-    #             text_list.append(str(selected_num) + ':' + df['姓名'][num_list.index(selected_num)] + '\n')
-    #             text = ''
-    #             for i in text_list:
-    #                 text += i
-    #             #shishi
-    #             locationx = 0.5* height*len(text_list)
-    #             drawText(text,[0,locationx -xiaxian])
-    #             print(text,locationx)
-    #             win.flip()
-    #             time.sleep(stop_time)
-    #             is_add = True
-    #         elif is_add == True:
-    #             locationx += move_juli
-    #             #shishi
-    #             drawText(text,[0,locationx -xiaxian])
-    #             print(text,locationx)
-    #             win.flip()
-    #             time.sleep(stop_time)
-    #             is_add = False
-
-    # elif len(text_list) == chengxianhangshu:
     if locationx %(height/2) != 0 :
         # if is_add == True:
         locationx += move_juli
--- test.py
+++ test.py
@@ -3,19 +3,46 @@
 from psychopy import visual, core, event, gui
 import random,os,time
 import pandas as pd
-
-height = 50
-# 中文字体
-text_font = 'SimSun'
-win = visual.Window([1920,1080],fullscr=True,color='#D0D0D0',units='pix')
-def drawimage(image1,location = [-300,0]):
-    pic = visual.ImageStim(win, image= image1, pos=location)
-    pic.draw(win)
-def drawText(text,location=[0,0],color=[-1, -1, -1]):
-    text = visual.TextStim(win, text=text, pos=location,color=[-1, -1, -1],height=height,font=text_font)
-    text.draw(win)
-
-# drawText('1 \n 2',location=[0,0],color=[-1, -1, -1])
-drawText('111111111111111\n22222222222222\n333333333333',location=[0,0],color=[-1, -1, -1])
-win.flip()
-time.sleep(3)
+# 获得某路径csv文件名list
+def file_name(file_dir):
+    L=[]
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.csv':
+                # L.append(os.path.join(root, file))
+                L.append(os.path.splitext(file)[0])
+    return L
+# 老师选择一下嘛
+def Get_Class(class_list):
+    myDlg = gui.Dlg(title="Choice Class")
+    myDlg.addText("---------------Choice Class-------------")
+    myDlg.addField('Class:',choices=class_list)
+    myDlg.show()
+    if myDlg.OK:
+        thisInfo = myDlg.data
+    else:
+        exit(0)
+    choiced_class = {}
+    choiced_class['Class'] = thisInfo[0]
+    return choiced_class
+path = os.path.abspath('.')
+class_list = file_name(path)
+choiced_class = Get_Class(class_list)
+df = pd.read_csv(path+'/num_file/'+ choiced_class['Class'] + '.csv', encoding='utf-8-sig')
+print(df)
+#
+# height = 50
+# # 中文字体
+# text_font = 'SimSun'
+# win = visual.Window([1920,1080],fullscr=True,color='#D0D0D0',units='pix')
+# def drawimage(image1,location = [-300,0]):
+#     pic = visual.ImageStim(win, image= image1, pos=location)
+#     pic.draw(win)
+# def drawText(text,location=[0,0],color=[-1, -1, -1]):
+#     text = visual.TextStim(win, text=text, pos=location,color=[-1, -1, -1],height=height,font=text_font)
+#     text.draw(win)
+#
+# # drawText('1 \n 2',location=[0,0],color=[-1, -1, -1])
+# drawText('111111111111111\n22222222222222\n333333333333',location=[0,0],color=[-1, -1, -1])
+# win.flip()
+# time.sleep(3)

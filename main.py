e03094599ea12ca7f2bf72318c64633ebbb54adc
Guo Wenjiao <1767397736@qq.com>
2020-08-21 14:11:24 +0800

    排版修改

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def drawimage(image1,size,location = [-290,0],opacity =1):
     pic = visual.ImageStim(win, image= image1, pos=location,size = (size[0], size[1]),opacity = opacity)
     pic.draw(win)
 def drawText(text,location=[0,0],color=[-1, -1, -1],height= 150):
-    text = visual.TextStim(win, text=text, pos=location,color=color,height=height,font=text_font)
+    text = visual.TextStim(win, text=text, pos=location,color=color,height=height,font=text_font,bold=True)
     text.draw(win)
 # 选择、读取文件
 path = os.path.abspath('.')
@@ -55,19 +55,21 @@ text_font = 'SimSun'  # 字体
 winsize = [1920,1080] # 窗口大小
 win = visual.Window(winsize,fullscr=True,color='#D0D0D0',units='pix')
 text_list = []
+#--------------------------------------------------------------
 # 调试时的一些参数
 duration = 10 # 倒计时持续时间
 chengxianhangshu = 1 # 屏幕呈现多少行字
 height = 150 # 字有多大
 move_juli = 5 #每次移动的距离
-count_down_location = [-650,300] # 倒计时的位置
+count_down_location = [-0,-50] # 倒计时的位置
 text_x = -370  # text的横坐标
 stop_time = 0.0016 # 刷新到下一屏的间隔时间
 # 循环前的一些参数和列表的填充
 time_gap = 0
 locationy = 0  # 最初的文字的纵坐标
 # xiaxian = 0.5 * ((chengxianhangshu)*height)# 最最下方文字的y坐标
-xiaxian = 300
+xiaxian = -200
+#--------------------------------------------------
 is_add = False # 该循环是否添加新的一行
 facted_secong = [] # 秒数取整用于比较
 for i in range(duration+1):
@@ -78,14 +80,15 @@ for i in range(chengxianhangshu):
     text_list.append('\n')
 t1 = time.clock()
 for i in range(3000):
-    drawimage('zhini.png',[857,1078],[0,0],opacity = 0.6)
-    drawimage('kuang.png',[1400,height + 20],[0,-0.5*xiaxian],opacity = 0.6)
+    drawimage('zhini.png',[857*0.5,1078*0.5],[-500,-150],opacity = 1)
+    drawimage('zhini.png',[857*0.5,1078*0.5],[500,-150],opacity = 1)
+    drawimage('kuang.png',[1400,height + 20],[0,height-xiaxian],opacity = 0.6)
     t2 = time.clock()
     time_gap = t2 - t1
     if int(time_gap) in facted_secong:
-        drawText(str(facted_secong[-( int(time_gap)+1)]),[count_down_location[0],count_down_location[1]],'red',400)
+        drawText(str(facted_secong[-( int(time_gap)+1)]),[count_down_location[0],count_down_location[1]],'red',500)
     if str(facted_secong[-( int(time_gap)+1)])== '0':
-        drawText('0',[count_down_location[0],count_down_location[1]],'red',400) #为了补上暂停时的一秒的画面)
+        drawText('0',[count_down_location[0],count_down_location[1]],'red',500) #为了补上暂停时的一秒的画面)
         drawText(text,[text_x,locationy-xiaxian])
         win.flip()
         # win.flip()
@@ -108,7 +111,6 @@ for i in range(3000):
     text = ''
     for i in text_list:
         text += i
-    #shishi
     locationy = 0.5*  height*len(text_list)
     drawText(text,[text_x,locationy -xiaxian])
     win.flip()
